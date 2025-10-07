1c65f2871b4c5718a5dfef31424129b41c8b679e
Amir Maghami <Maghami.A1987@gmail.com>
2025-10-07 18:41:51 +0330

    Update panel.ts

--- src/app/layouts/panel/panel.ts
+++ src/app/layouts/panel/panel.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-panel',
-  imports: [RouterOutlet, MatButtonModule],
+  imports: [RouterOutlet, MatButtonModule, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './panel.html',
   styleUrl: './panel.scss',
 })
